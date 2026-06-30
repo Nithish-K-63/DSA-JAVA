32161a0b9c2e590947689db868ccea65a8a2d0e2
Nithish-K-63 <nithishk63741@gmail.com>
2026-06-30 12:11:59 +0530

    [141] [Time Beats: 100.00%] [Memory Beats: 32.86%] - LeetPush

--- Linked-List-Cycle.java
+++ Linked-List-Cycle.java
@@ -9,18 +9,34 @@
 9 *     }
 10 * }
 11 */
-12public class Solution {
-13    public boolean hasCycle(ListNode head) {
-14        ListNode temp=head;
-15        HashMap<ListNode,Boolean> mp=new HashMap<>();
-16        while(temp!=null){
-17            if(mp.containsKey(temp)){
-18                return true;
-19            }
-20            mp.put(temp,true);
-21            temp=temp.next;
-22        }
-23        return false;
-24    }
-25}//TC:O(N)
+12// public class Solution {
+13//     public boolean hasCycle(ListNode head) {
+14//         ListNode temp=head;
+15//         HashMap<ListNode,Boolean> mp=new HashMap<>();
+16//         while(temp!=null){
+17//             if(mp.containsKey(temp)){
+18//                 return true;
+19//             }
+20//             mp.put(temp,true);
+21//             temp=temp.next;
+22//         }
+23//         return false;
+24//     }
+25// }//TC:O(N)
 26//SC:O(N)
+27public class Solution {
+28    public boolean hasCycle(ListNode head) {
+29        ListNode fast=head;
+30        ListNode slow=head;
+31        while(fast!=null&& fast.next!=null){
+32           slow=slow.next;
+33           fast=fast.next.next;
+34           if(slow==fast){
+35            return true;
+36           }
+37        }
+38        return false;
+39    }
+40}
+41
+42
