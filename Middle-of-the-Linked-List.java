2bc9a634ef1fa6e2cc7913faab41e238846a48de
Nithish-K-63 <nithishk63741@gmail.com>
2026-06-30 11:33:00 +0530

    [876] [Time Beats: 100.00%] [Memory Beats: 85.83%] - LeetPush

--- Middle-of-the-Linked-List.java
+++ Middle-of-the-Linked-List.java
@@ -8,18 +8,30 @@
 8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 9 * }
 10 */
-11class Solution {
-12    public ListNode middleNode(ListNode head) {
-13        int n=0;
-14        ListNode temp=head;
-15        while(temp!=null){
-16            temp=temp.next;
-17            n++;
-18        }
-19        int middle=n/2;
-20        temp=head;
-21        for(int i=0;i<middle;i++){
-22            temp=temp.next;
-23        }return temp;
-24    }
-25}
+11// class Solution {
+12//     public ListNode middleNode(ListNode head) {
+13//         int n=0;
+14//         ListNode temp=head;
+15//         while(temp!=null){
+16//             temp=temp.next;
+17//             n++;
+18//         }
+19//         int middle=n/2;
+20//         temp=head;
+21//         for(int i=0;i<middle;i++){
+22//             temp=temp.next;
+23//         }return temp;
+24//     }
+25// }//TC:O(N)
+26// OPTIMIZTION:
+27class Solution {
+28    public ListNode middleNode(ListNode head) {
+29        ListNode fast=head;
+30        ListNode slow=head;
+31        while(fast!=null&& fast.next!=null){
+32            slow=slow.next;
+33            fast=fast.next.next;
+34        }
+35        return slow;
+36    }
+37}
