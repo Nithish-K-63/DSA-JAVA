a8367d34cff61e812557bd3c342195b7a29f9efb
Nithish K <nithishk63741@gmail.com>
2026-09-21 20:29:41 +0530

    [3498] [Time Beats: 100.00%] [Memory Beats: 80.81%] - LeetPush

---
+++ 3498-Reverse-Degree-of-a-String.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int ans = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            int n = -(s.charAt(i) - 'z' - 1);
+            ans += n * (i + 1);
+        }
+
+        return ans;
+    }
+}
